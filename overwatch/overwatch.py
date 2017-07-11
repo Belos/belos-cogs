cdf864ffc25b685b708f0eb583f8a2aecb5c89c7
Belos <Belos@users.noreply.github.com>
2017-07-11 13:38:17 +0300

    Add files via upload

--- overwatch/overwatch.py
+++ overwatch/overwatch.py
@@ -3,11 +3,8 @@ from discord.ext import commands
 from random import randint
 import aiohttp
 
-__author__ = "Belos"
-
-
 class Overwatch:
-    """Statistics for Overwatch"""
+    """Statistics for Overwatch Cog"""
 
     def __init__(self, bot):
         self.bot = bot
